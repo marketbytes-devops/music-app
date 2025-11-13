6cc1558ab2b1868dc5dab62208c74845d1821fc2
akshaysambhu <your_email@example.com>
2025-11-13 17:40:24 +0530

    section

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import Section1 from "./SectionOne"
 import Section2 from "./SectionTwo"
+import SectionSix from "./SectionSix"
 
 const HomePage = () => {
   return (
     <div>
       <Section1/>
       <Section2/>
+      <SectionSix/>
     </div>
   )
 }
